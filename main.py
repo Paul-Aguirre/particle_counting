4b5de2d002a57cd2b288aef3a1e82ac0ec17bdd7
Paul Aguirre <paul.aguirre@univ-grenoble-alpes.fr>
2024-07-13 04:28:16 +0200

    Added particle concentration computation to main.py

--- main.py
+++ main.py
@@ -22,5 +22,20 @@ multi_slice_viewer(
 
 plt.show()
 
+total_volume = (
+    abs(metadata["z_coordinates"][-1] - metadata["z_coordinates"][0])
+    * metadata["width"]
+    * metadata["height"]
+    * metadata["pixel_microns"] ** 2
+)
+
+num_particles = len(results["props"])
+
+particle_concentration = num_particles / total_volume
+
+print(f"Total volume analysed: {total_volume:.3f} µm^3")
+print(f"Number of particles detected: {num_particles}")
+print(f"Particle concentration: {particle_concentration:.7f} particles/µm^3")
+
 # if __name__ == "__main__":
 #     main()
