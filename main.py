6a25ec52cb83531f70fe9d760190b2f8e9f2daba
Paul Aguirre <paul.aguirre@univ-grenoble-alpes.fr>
2024-08-01 19:30:41 +0200

    Converted script in main.py to function main()

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from tkinter.filedialog import askopenfilename
+import tomllib
+from pathlib import Path
 
 import numpy as np
 from matplotlib import pyplot as plt
@@ -7,115 +9,142 @@ from load_image_stack import load_image_stack
 from process_stack import process_stack
 from multi_slice_viewer import multi_slice_viewer
 from particle_distributions import particle_distributions
-from utils import z_resolution
+from utils import z_resolution, initialize_toml
 
 
-# def main() -> None:
-path = askopenfilename()
+def main(datapath: str | Path) -> None:
+    datapath = Path(datapath)
+    configpath = datapath.parent / f"{datapath.stem}.toml"
 
-image_stack, metadata = load_image_stack(path)
+    # todo: à extraire dans une fonction séparée
+    # todo -------------------------------------------------------------
+    if not configpath.exists():
+        _, metadata = load_image_stack(str(datapath))
+        initialize_toml(configpath, metadata)
+        print(f"Created {configpath.name}.")
+        print("Please edit the configuration file.")
+        input("Press Enter to continue.")
+    # todo -------------------------------------------------------------
 
-results = process_stack(image_stack, metadata, 1, full_bbox=False)
+    with open(configpath, "rb") as f:
+        config = tomllib.load(f)
 
-multi_slice_viewer(
-    # volume=results["binary"],
-    volume=image_stack,
-    bboxes=results["bboxes3d"],
-    bbox_alpha=0.5,
-)
-
-plt.show()
-
-total_volume = (
-    abs(metadata["z_coordinates"][-1] - metadata["z_coordinates"][0])
-    * metadata["width"]
-    * metadata["height"]
-    * metadata["pixel_microns"] ** 2
-)
-
-num_particles = len(results["props"])
-
-particle_concentration = num_particles / total_volume
-
-print(f"Total volume analysed: {total_volume:.4e} µm^3")
-print("{:-^72}".format("Particle number calculations in number"))
-print(f"Number of particles detected: {num_particles:.4e}")
-print(f"Particle concentration: {particle_concentration:.4e} particles/µm^3")
-
-x, y, z = particle_distributions(
-    results,
-    metadata,
-    bins_xy=20,
-    bins_z=20,
-    bins_pixels=100,
-    bins_area=100,
-    bins_diameter=100,
-    boxplot_config={"showfliers": False},
-    # verbose=True,
-)
-
-# particle number calculations in volume
-median_num_pixels = np.median(
-    np.array(
-        [prop.num_pixels for prop in results["props"]],
+    image_stack, metadata = load_image_stack(
+        str(datapath),
+        start=config["stack_start"],
+        stop=config["stack_stop"],
     )
-)
-nums_pixels = np.array([prop.num_pixels for prop in results["props"]])
-num_particles_in_volume = np.sum(nums_pixels) / median_num_pixels
-particle_concentration_in_volume = num_particles_in_volume / total_volume
 
-print("{:-^72}".format("Particle number calculations in volume"))
-# print("Without z-axis correction:")
-print(
-    f"Number of particles (computed in volume): {num_particles_in_volume:4e}",
-)
-print(
-    "Particle concentration (computed in volume):"
-    f"{particle_concentration_in_volume:4e} particles/µm^3"
-)
+    results = process_stack(image_stack, metadata, 1, full_bbox=False)
 
-# # computing correction factor for z-axis
-# median_diameter = np.mean(np.array([np.median(x), np.median(y)]))
+    multi_slice_viewer(
+        # volume=results["binary"],
+        volume=image_stack,
+        bboxes=results["bboxes3d"],
+        bbox_alpha=0.5,
+    )
 
-# results = process_stack(
-#     image_stack=image_stack,
-#     metadata=metadata,
-#     particle_diameter=1,
-#     full_bbox=False,
-#     spacing=(
-#         z_resolution(
-#             diameter=median_diameter,
-#             num_pixels=median_num_pixels,
-#             x_resolution=metadata["pixel_microns"],
-#             y_resolution=metadata["pixel_microns"],
-#         ),
-#         metadata["pixel_microns"],
-#         metadata["pixel_microns"],
-#     ),
-# )
+    # todo: rendre l'affichage des graphes optionnel
+    plt.show()
+
+    total_volume = (
+        abs(metadata["z_coordinates"][-1] - metadata["z_coordinates"][0])
+        * metadata["width"]
+        * metadata["height"]
+        * metadata["pixel_microns"] ** 2
+    )
+
+    num_particles = len(results["props"])
+
+    particle_concentration = num_particles / total_volume
+
+    print(f"File: {datapath.name}")
+    print(f"Total volume analysed: {total_volume:.4e} µm^3")
+    print("{:-^72}".format("Particle number calculations in number"))
+    print(f"Number of particles detected: {num_particles:.4e}")
+    print(f"Particle concentration: {particle_concentration:.4e} particles/µm^3")
+
+    # todo: sauvegarder les graphes
+    x, y, z = particle_distributions(
+        results,
+        metadata,
+        bins_xy=20,
+        bins_z=20,
+        bins_pixels=100,
+        bins_area=100,
+        bins_diameter=100,
+        boxplot_config={"showfliers": False},
+        # verbose=True,
+    )
+
+    # particle number calculations in volume
+    # ! le nombre de pixel médian est trop élevé pour obtenir un calcul
+    # ! correct de la concentration en particules en volume.
+    # ? Utiliser le fractile d'ordre 0.1 ou 0.2
+    median_num_pixels = np.median(
+        np.array(
+            [prop.num_pixels for prop in results["props"]],
+        )
+    )
+    nums_pixels = np.array([prop.num_pixels for prop in results["props"]])
+    num_particles_in_volume = np.sum(nums_pixels) / median_num_pixels
+    particle_concentration_in_volume = num_particles_in_volume / total_volume
+
+    print("{:-^72}".format("Particle number calculations in volume"))
+    # print("Without z-axis correction:")
+    print(
+        f"Number of particles (computed in volume): {num_particles_in_volume:.4e}",
+    )
+    print(
+        "Particle concentration (computed in volume):"
+        f"{particle_concentration_in_volume:.4e} particles/µm^3"
+    )
+
+    # # computing correction factor for z-axis
+    # median_diameter = np.mean(np.array([np.median(x), np.median(y)]))
+
+    # results = process_stack(
+    #     image_stack=image_stack,
+    #     metadata=metadata,
+    #     particle_diameter=1,
+    #     full_bbox=False,
+    #     spacing=(
+    #         z_resolution(
+    #             diameter=median_diameter,
+    #             num_pixels=median_num_pixels,
+    #             x_resolution=metadata["pixel_microns"],
+    #             y_resolution=metadata["pixel_microns"],
+    #         ),
+    #         metadata["pixel_microns"],
+    #         metadata["pixel_microns"],
+    #     ),
+    # )
+
+    # particle_volumes_corrected = np.array(
+    #     [prop.area for prop in results["spacing_corrected_props"]],
+    # )
+    # num_particles_in_volume_corrected = np.sum(particle_volumes_corrected) / np.median(
+    #     particle_volumes_corrected
+    # )
+    # particle_concentration_in_volume_corrected = (
+    #     num_particles_in_volume_corrected / total_volume
+    # )
+
+    # print("With z-axis correction:")
+    # print(
+    #     "Number of particles (computed in volume):"
+    #     f"{num_particles_in_volume_corrected:e}",
+    # )
+    # print(
+    #     "Particle concentration (computed in volume):"
+    #     f"{particle_concentration_in_volume_corrected:e} particles/µm^3"
+    # )
+
+    plt.show()
+
+    return image_stack, metadata, results
 
 
-# particle_volumes_corrected = np.array(
-#     [prop.area for prop in results["spacing_corrected_props"]],
-# )
-# num_particles_in_volume_corrected = np.sum(particle_volumes_corrected) / np.median(
-#     particle_volumes_corrected
-# )
-# particle_concentration_in_volume_corrected = (
-#     num_particles_in_volume_corrected / total_volume
-# )
-
-# print("With z-axis correction:")
-# print(
-#     "Number of particles (computed in volume):"
-#     f"{num_particles_in_volume_corrected:e}",
-# )
-# print(
-#     "Particle concentration (computed in volume):"
-#     f"{particle_concentration_in_volume_corrected:e} particles/µm^3"
-# )
-
-plt.show()
-
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    datapath = Path(askopenfilename(title="Choose a data file"))
+    image_stack, metadata, results = main(datapath)
